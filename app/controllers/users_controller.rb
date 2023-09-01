f22ed89cb75060740c35020cca00d9ead2f8ea22
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-01 22:25:31 +0300

    change the port in puma.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     end
   
     def create
-      @user = User.new(user_params)
+      @user = User.create(user_params)
       if @user.save
         render json: @user, status: :created
       else
