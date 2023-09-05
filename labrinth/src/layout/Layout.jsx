1b59c342d704e1d6c7d93483f38c820c5f424141
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-05 10:33:31 +0300

    create Layout file for the dashboard

--- labrinth/src/layout/Layout.jsx
+++ labrinth/src/layout/Layout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from '../pages/NavBar'
 import { Outlet } from 'react-router'
+import Header from '../components/Header'
+import SideBar from '../components/SideBar'
 
 function Layout() {
   return (
-    <div>
-        <NavBar />
+    <div className='grid-container'>
+       <Header />
+       <SideBar />
         <Outlet />
     </div>
   )
