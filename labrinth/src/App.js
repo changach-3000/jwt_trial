c7345c916e6de631e717288ee9a5f90ef4c31d3a
changach-3000 <sharon.changach@student.moringaschool.com>
2023-09-05 11:28:39 +0300

    add a route for main

--- labrinth/src/App.js
+++ labrinth/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Layout from "./layout/Layout"
 import LandingPage from "./pages/LandingPage";
+import Main from "./components/Main";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout/>}>
               <Route index element={<LandingPage/>}/>
+              <Route path="main" element={<Main/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
